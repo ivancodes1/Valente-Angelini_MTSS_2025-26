1ea139d246bdc5d978505649a166f718a92fb334
Ivan <74571420+ivancodes1@users.noreply.github.com>
2026-05-07 23:57:26 +0200

    Implementata conversione fino a 20 - Close #9

--- roman-number/src/test/java/it/unipd/mtss/IntegerToRomanTest.java
+++ roman-number/src/test/java/it/unipd/mtss/IntegerToRomanTest.java
@@ -7,72 +7,56 @@ package it.unipd.mtss;
 
 import static org.junit.Assert.assertEquals;
 
+import java.util.Arrays;
+import java.util.Collection;
+
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
 
+/* essendo che stiamo aumentando i numeri da convertire (e quindi da controllare)
+meglio parametrizzare come visto a lezione per non fare troppi test singoli*/
+@RunWith(Parameterized.class)
 public class IntegerToRomanTest {
-    @Test
-    public void testNumbersOneToThree() {
-        String expected = "";
-        for (int num = 1; num <= 3; ++num) {
-            expected += "I";
-            String actual = IntegerToRoman.convert(num);
-            assertEquals(expected, actual);
-        }
+    private int input;
+    private String expected;
+    public IntegerToRomanTest(int input, String expected) {
+        this.input = input;
+        this.expected = expected;
+    }
+
+    // tabella dei dati per la parametrizzazione
+    @Parameters(name = "Test: convert({0}) = {1}")
+    public static Collection<Object[]> data() {
+        return Arrays.asList(new Object[][] {
+            { 1, "I" }, { 3, "III" }, // unità base
+            { 4, "IV" }, { 5, "V" }, // sottrazione e limite 5
+            { 8, "VIII" }, { 9, "IX" }, // unità alte e sottrazione 9
+            { 10, "X" }, // decina esatta
+            { 14, "XIV" }, { 15, "XV" }, // decina + sottrazione/limite
+            { 19, "XIX" }, { 20, "XX" } // limiti superiori
+        });
     }
 
     @Test
-    public void testNumberFour() {
-        int input = 4;
-        String expected = "IV";
-        String actual = IntegerToRoman.convert(input);
-        assertEquals(expected, actual);
-    }
-
-    @Test
-    public void testNumberFive() {
-        int input = 5;
-        String expected = "V";
-        String actual = IntegerToRoman.convert(input);
-        assertEquals(expected, actual);
-    }
-
-    @Test
-    public void testNumbersSixToEight() {
-        String expected = "V";
-        for (int num = 6; num <= 8; ++num) {
-            expected += "I";
-            String actual = IntegerToRoman.convert(num);
-            assertEquals(expected, actual);
-        }
-    }
-
-    @Test
-    public void testNumberNine() {
-        int input = 9;
-        String expected = "IX";
-        String actual = IntegerToRoman.convert(input);
-        assertEquals(expected, actual);
-    }
-
-    @Test
-    public void testNumberTen() {
-        int input = 10;
-        String expected = "X";
+    public void testNumbersOneToTwenty() {
+        //qui l'arrange è "implicito": i dati sono già preparati dal costruttore parametrizzato.
         String actual = IntegerToRoman.convert(input);
         assertEquals(expected, actual);
     }
 
     @Test(expected = IllegalArgumentException.class)
     public void testZeroThrowsException() {
-        int input = 0;
-        IntegerToRoman.convert(input);
-		//qui l'assert è gestito implicitamente da "(expected = ...)"
+        int outOfBoundsInput = 0;
+        IntegerToRoman.convert(outOfBoundsInput);
+        //qui l'assert è gestito implicitamente da "(expected = ...)"
     }
 
     @Test(expected = IllegalArgumentException.class)
-    public void testElevenThrowsException() {
-        int input = 11;
-        IntegerToRoman.convert(input);
-		//qui l'assert è gestito implicitamente da "(expected = ...)"
+    public void testTwentyOneThrowsException() {
+        int outOfBoundsInput = 21;
+        IntegerToRoman.convert(outOfBoundsInput);
+        //qui l'assert è gestito implicitamente da "(expected = ...)"
     }
 }
