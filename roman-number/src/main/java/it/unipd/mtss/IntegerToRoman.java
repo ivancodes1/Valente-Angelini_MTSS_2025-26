b809d9648051fcd4ea9ca4ebea4755774fe39304
matt154154 <matteo.angelini.154@gmail.com>
2026-05-09 11:38:17 +0200

    Implementata conversione fino a 50

--- roman-number/src/main/java/it/unipd/mtss/IntegerToRoman.java
+++ roman-number/src/main/java/it/unipd/mtss/IntegerToRoman.java
@@ -8,25 +8,30 @@ package it.unipd.mtss;
 public class IntegerToRoman {
     
     public static String convert(int number) throws IllegalArgumentException {
-        if (number < 1 || number > 20) {
-            throw new IllegalArgumentException(number + " is not valid. Must be > 0 and <= 20");
+        if (number < 1 || number > 50) {
+            throw new IllegalArgumentException(number + " is not valid. Must be > 0 and <= 50");
         }
         String result = "";
+        if (number == 50) {
+            result += "L";
+            number -= 50;
+        } else if (number >= 40) {
+            result += "XL";
+            number -= 40;
+        }
         while (number >= 10) {
             result += "X";
             number -= 10;
         }
-        if (number > 0) {
-            if (number % 5 == 4) {
-                result += "I" + ((number == 9) ? "X" : "V");
-            } else {
-                if (number >= 5) {
-                    result += "V";
-                    number -= 5;
-                }
-                for (int i = 0; i < number; ++i) {
-                    result += "I";
-                }
+        if (number % 5 == 4) {
+            result += "I" + ((number == 9) ? "X" : "V");
+        } else {
+            if (number >= 5) {
+                result += "V";
+                number -= 5;
+            }
+            for (int i = 0; i < number; ++i) {
+                result += "I";
             }
         }
         return result;
